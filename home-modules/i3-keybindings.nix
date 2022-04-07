276c37813057f38997e05163f148d3e342e1d460
Nick Anthony Flueckiger <nick.flueckiger@renuo.ch>
2022-04-07 14:52:34 +0200

    Add keybindings

--- home-modules/i3-keybindings.nix
+++ home-modules/i3-keybindings.nix
@@ -1,4 +1,28 @@
 mod: {
   "${mod}+d" = "exec rofi -show run";
   "${mod}+q" = "kill";
+
+  "${mod}+Return" = "exec alacritty";
+
+  "${mod}+h" = "split h";
+  "${mod}+v" = "split v";
+  "${mod}+f" = "fullscreen";
+  "${mod}+s" = "layout stacking";
+  "${mod}+t" = "layout tabbed";
+  "${mod}+e" = "layout default";
+
+
+  "${mod}+1" = "workspace 1";
+  "${mod}+2" = "workspace 2";
+  "${mod}+3" = "workspace 3";
+
+  "${mod}+Shift+H" = "move left";
+  "${mod}+Shift+J" = "move down";
+  "${mod}+Shift+K" = "move up";
+  "${mod}+Shift+L" = "move right";
+
+  "${mod}+Shift+Left" = "move left";
+  "${mod}+Shift+Down" = "move down";
+  "${mod}+Shift+Up" = "move up";
+  "${mod}+Shift+Right" = "move right";
 }
