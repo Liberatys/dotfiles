3b8a2d05d3903ba3c1d2b75c5e77de1056dade40
Liberatys <nick.flueckiger@renuo.ch>
2022-05-21 16:45:26 +0200

    Some bindings and packages from kubecon

---
+++ home-modules/devops.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+{
+  options = {
+    dotfiles.devops.enabled = mkEnableOption "devops";
+  };
+
+  # All applications needed for devops operatoins / observation and work
+  config = mkIf config.dotfiles.devops.enabled {
+    home.packages = with pkgs; [
+      act
+      dagger
+      terraform
+      doctl
+      fluxcd
+      minikube
+      kubectl
+      kube3d
+      kubeseal
+      docker-machine-hyperkit
+      lens
+      ngrok
+    ];
+  };
+}
